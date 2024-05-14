e0618e4e0420486cb13b97028bea941856b9dd90
Ali Mansoor <alimansoor4493@gmail.com>
2024-05-14 12:59:46 +0500

    error solved

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import the Link component
 import './Card.css';
 
 const Card = ({ image, name, price, id }) => (
-    <Link to={`/phonedetails/${id}`}> {/* Wrap the card in a Link component */}
         <div className="card">
             <img src={image} alt={name} />
             <h2>{name}</h2>
             <p>{price}</p>
         </div>
-    </Link>
 );
 
 export default Card;
