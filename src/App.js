288896e02b787c47d550be8d5467c9451e84b24c
SMuhammadMustafa <mmustafa1444@gmail.com>
2024-05-15 17:05:37 +0500

    feat: Add Create Listing and Phone Comparison pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import CreateListing from './components/CreateListing';
+import PhoneComparison from './components/PhoneComparison';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      {/* <Dashboard /> */}
+      {/* <CreateListing /> */}
+    <PhoneComparison />
     </div>
   );
 }
---
+++ src/components/PhoneComparison.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { createTheme } from '@mui/material/styles';
+import './PhoneComparison.css'; // Import the CSS file
+
+
+import {
+  Container,
+  Grid,
+  Paper,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Table,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Typography,
+} from '@mui/material';
+
+const PhoneComparison = () => {
+  const [selectedPhones, setSelectedPhones] = useState([]);
+
+  const handlePhoneSelect = (event) => {
+    const selected = event.target.value;
+    setSelectedPhones(selected);
+  };
+
+  const phones = [
+    { id: 1, name: 'Phone A', brand: 'Brand X', ram: '6GB', storage: '128GB' },
+    { id: 2, name: 'Phone B', brand: 'Brand Y', ram: '8GB', storage: '256GB' },
+    { id: 3, name: 'Phone C', brand: 'Brand Z', ram: '4GB', storage: '64GB' },
+  ];
+
+  return (
+    <Container maxWidth="lg" style={{ marginTop: 20 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel>Select Phones to Compare</InputLabel>
+            <Select
+              multiple
+              value={selectedPhones}
+              onChange={handlePhoneSelect}
+              renderValue={(selected) => selected.join(', ')}
+            >
+              {phones.map((phone) => (
+                <MenuItem key={phone.id} value={phone.name}>
+                  {phone.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        {selectedPhones.length > 1 && (
+          <Grid item xs={12}>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Feature</TableCell>
+                    {selectedPhones.map((phone) => (
+                      <TableCell key={phone} align="center">
+                        {phone}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Brand</TableCell>
+                    {selectedPhones.map((phone) => (
+                      <TableCell key={phone} align="center">
+                        {phones.find((p) => p.name === phone)?.brand}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>RAM</TableCell>
+                    {selectedPhones.map((phone) => (
+                      <TableCell key={phone} align="center">
+                        {phones.find((p) => p.name === phone)?.ram}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Storage</TableCell>
+                    {selectedPhones.map((phone) => (
+                      <TableCell key={phone} align="center">
+                        {phones.find((p) => p.name === phone)?.storage}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                  {/* Add more rows for other specifications */}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        )}
+        {selectedPhones.length <= 1 && (
+          <Grid item xs={12}>
+            <Typography variant="body1" align="center">
+              Select at least two phones to compare.
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
+};
+
+export default PhoneComparison;
