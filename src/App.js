78d508ca2b2d8fc35c9b8f17d71cf4fcd5533e8e
Ali Mansoor <alimansoor4493@gmail.com>
2024-05-16 22:33:07 +0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import PhoneComparison from './components/js/PhoneComparison';
 import './App.css';
 import Login from './components/login';
 import SignUp from './components/Signup'; // Create SignUp component if not already done
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MessagePage from './components/Messagepage';
 
 
